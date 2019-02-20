ca9f3eb6a9268e9cd02ea3014cf1ed33da52fa8b
Sam Eagen <sameagen@Sams-MacBook-Pro.local>
2019-02-19 20:24:43 -0600

    Added in Pair.java

---
+++ Pair.java
@@ -0,0 +1,22 @@
+/**
+ * Pair class.
+ * Written by others.
+ */
+public class Pair<T,U> {
+  T left;
+  U right;
+
+  public Pair(T left, U right) {
+    this.left = left;
+    this.right = right;
+  } // Pair
+
+  public T getLeft() {
+    return this.left;
+  }
+
+  public U getRight() {
+    return this.right;
+  }
+} // class Pair
+
