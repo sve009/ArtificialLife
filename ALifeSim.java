21ced9044386f69e2ef61c39e6e92bba8bf95e9c
Sam Eagen <sameagen@Sams-MacBook-Pro.local>
2019-02-20 09:27:00 -0600

    Code refactoring

--- ALifeSim.java
+++ ALifeSim.java
@@ -1,35 +1,45 @@
+import java.io.PrintWriter;
 
-
+/**
+ * The driver class.
+ * Runs different experiments and
+ * prints the results.
+ */
 public class ALifeSim {
-        public static void main(String[] args) {
-                if (args.length != 4) {
-                        throw new Error("Invalid number of arguments");
-                }
 
-                int iterations = Integer.parseInt(args[0]);
+  //main----------------------------------------
 
-                int cooperators = Integer.parseInt(args[1]);
-                int defectors = Integer.parseInt(args[2]);
-                int partialCooperators = Integer.parseInt(args[3]);
+  public static void main(String[] args) {
+    PrintWriter pen = new PrintWriter(System.out, true);
 
-                Pair<String, Integer>[] startingNumbers = (Pair<String, Integer>[]) (new Pair[3]);
+    if (args.length != 4) {
+      throw new Error("Invalid number of arguments");
+    }
 
-                startingNumbers[0] = new Pair<String, Integer>("Cooperator", cooperators);
-                startingNumbers[1] = new Pair<String, Integer>("Defector", defectors);
-                startingNumbers[2] = new Pair<String, Integer>("PartialCooperator", partialCooperators);
+    int iterations = Integer.parseInt(args[0]);
 
-                Population pop = new Population(startingNumbers);
+    int cooperators = Integer.parseInt(args[1]);
+    int defectors = Integer.parseInt(args[2]);
+    int partialCooperators = Integer.parseInt(args[3]);
 
-                for (int i = 0; i < iterations; i++) {
-                        pop.update();
-                }
+    Pair<String, Integer>[] startingNumbers = (Pair<String, Integer>[]) (new Pair[3]);
 
-                Pair<String, Integer>[] finalNumbers = pop.getPopulationCounts();
+    startingNumbers[0] = new Pair<String, Integer>("Cooperator", cooperators);
+    startingNumbers[1] = new Pair<String, Integer>("Defector", defectors);
+    startingNumbers[2] = new Pair<String, Integer>("PartialCooperator", partialCooperators);
 
-                System.out.println("After " + iterations + " ticks:");
-                System.out.println("Cooperators = " + finalNumbers[0].getRight());
-                System.out.println("Defectors = " + finalNumbers[1].getRight());
-                System.out.println("Partial Cooperators = " + finalNumbers[2].getRight());
-                System.out.println("\nMean Cooperation Probability = " + pop.calculateCooperationMean());
-        }
+    Population pop = new Population(startingNumbers);
+
+    for (int i = 0; i < iterations; i++) {
+      pop.update();
+    }
+
+    Pair<String, Integer>[] finalNumbers = pop.getPopulationCounts();
+
+    pen.println("After " + iterations + " ticks:");
+    pen.println("Cooperators = " + finalNumbers[0].getRight());
+    pen.println("Defectors = " + finalNumbers[1].getRight());
+    pen.println("Partial Cooperators = " + finalNumbers[2].getRight());
+    pen.println("\nMean Cooperation Probability = " + pop.calculateCooperationMean());
+  }
 }
